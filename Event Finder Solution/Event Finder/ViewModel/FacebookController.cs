6e691a00da7088aebe7114b8c3a6ef32b3baeef3
Ahmad Abbad <ahm.abbad@gmail.com>
2013-11-18 12:42:31 +0000

    refactored code

--- Event Finder Solution/Event Finder/ViewModel/FacebookController.cs
+++ Event Finder Solution/Event Finder/ViewModel/FacebookController.cs
@@ -11,7 +11,7 @@ namespace Event_Finder.ViewModel
     class FacebookController
     {
 
-        async public Task<Data> GetEventsFromFacebook(double offset, double latitude, double longitude, DateTime dt) 
+        async public Task<Data> GetEventsFromFacebook(double offset, double latitude, double longitude, double dt) 
         {
             String result = await CallFacebookApiForEvents(offset, latitude, longitude, dt);
             return ParseEvents(result);
@@ -23,15 +23,21 @@ namespace Event_Finder.ViewModel
             return JsonConvert.DeserializeObject<Data>(jsonListOfEvents);
         }
 
-        async private Task<string> CallFacebookApiForEvents(double offset, double latitude, double longitude, DateTime dt)
+        private String makeQuery(double offset, double latitude, double longitude, double dt) 
         {
-            var fb = new Facebook.FacebookClient(App.FacebookSessionClient.CurrentSession.AccessToken);
-
-            string query = String.Format(@"SELECT eid, name, description, venue FROM event WHERE contains('amman') AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time < now() LIMIT 40",
+            return String.Format(@"SELECT eid, name, description, venue FROM event WHERE contains('amman') OR contains('irbid') AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time > ""{4}"" LIMIT 40",
                                        (offset - latitude).ToString(),
                                        (offset + latitude).ToString(),
                                        (offset - longitude).ToString(),
-                                       (offset + longitude).ToString());
+                                       (offset + longitude).ToString(),
+                                       dt.ToString());        
+        }
+
+        async private Task<string> CallFacebookApiForEvents(double offset, double latitude, double longitude, double dt)
+        {
+            var fb = new Facebook.FacebookClient(App.FacebookSessionClient.CurrentSession.AccessToken);
+
+            string query = makeQuery(offset, latitude, longitude, dt); 
 
             var result = await fb.GetTaskAsync("fql",
                 new
