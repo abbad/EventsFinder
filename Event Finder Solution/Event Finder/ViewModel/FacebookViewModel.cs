9576b2b6fa6ad3867436ffbb3622dc2d85383311
Ahmad Abbad <ahm.abbad@gmail.com>
2014-01-11 21:18:26 +0300

    added equals to the query

--- Event Finder Solution/Event Finder/ViewModel/FacebookViewModel.cs
+++ Event Finder Solution/Event Finder/ViewModel/FacebookViewModel.cs
@@ -141,7 +141,7 @@ namespace Event_Finder.ViewModel
 
         private String MakeQueryWithContains(double offset, double latitude, double longitude, double dt, double dtEndRange, String searchString)
         {
-            return String.Format(@"SELECT eid, start_time, end_time, pic_big, pic_square, name, description, venue FROM event WHERE contains('""{5}""') AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time > ""{4}"" and start_time < ""{6}"" ORDER BY start_time ASC",
+            return String.Format(@"SELECT eid, start_time, end_time, pic_big, pic_square, name, description, venue FROM event WHERE contains('""{5}""') AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time >= ""{4}"" and start_time <= ""{6}"" ORDER BY start_time ASC",
                                        (latitude - offset).ToString(),
                                        (latitude + offset).ToString(),
                                        (longitude- offset).ToString(),
@@ -159,7 +159,7 @@ namespace Event_Finder.ViewModel
 
         private String MakeQueryForMeAndFriendsEvents(double offset, double latitude, double longitude, double dt, double dtEndRange)
         {
-            return String.Format(@"SELECT eid, start_time, end_time, pic_big,pic_square, name, description, venue FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) OR uid = me()) AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time > ""{4}"" and start_time < ""{5}""  ORDER BY start_time ASC LIMIT 50 ", 
+            return String.Format(@"SELECT eid, start_time, end_time, pic_big,pic_square, name, description, venue FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid IN (SELECT uid2 FROM friend WHERE uid1 = me()) OR uid = me()) AND venue.latitude > ""{0}"" AND venue.latitude < ""{1}"" AND venue.longitude > ""{2}"" AND venue.longitude < ""{3}"" AND start_time >= ""{4}"" and start_time <= ""{5}""  ORDER BY start_time ASC LIMIT 50 ", 
                                        (latitude - offset).ToString(),
                                        (latitude + offset).ToString(),
                                        (longitude - offset).ToString(),
@@ -171,7 +171,7 @@ namespace Event_Finder.ViewModel
 
         private String MakeQueryForUserEvents(double dt, double dtEndRange) 
         {
-            return String.Format(@"SELECT eid, start_time, end_time, pic_big, pic_square, name, description, venue FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid = me()) and start_time > {0} and start_time < {1}", dt.ToString(),
+            return String.Format(@"SELECT eid, start_time, end_time, pic_big, pic_square, name, description, venue FROM event WHERE eid IN (SELECT eid FROM event_member WHERE uid = me()) and start_time >= {0} and start_time <= {1}", dt.ToString(),
                                        dtEndRange.ToString());
         }
 
